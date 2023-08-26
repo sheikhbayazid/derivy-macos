479358b23802c26947dd91dd37906f973e4aef74
Sheikh Bayazid <sheikhbayazid@gmail.com>
2023-08-26 02:38:23 +0200

    Add menu bar

--- Derivy/DerivyApp.swift
+++ Derivy/DerivyApp.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 @main
 struct DerivyApp: App {
+    @State var currentNumber: String = "1"
+
     var body: some Scene {
-        WindowGroup {
+        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+             Button("One") {
+                 currentNumber = "1"
+             }
+             .keyboardShortcut("1")
+             Button("Two") {
+                 currentNumber = "2"
+             }
+             .keyboardShortcut("2")
+             Button("Three") {
+                 currentNumber = "3"
+             }
+             .keyboardShortcut("3")
+
             ContentView()
-        }
+
+             Divider()
+             Button("Quit") {
+                 NSApplication.shared.terminate(nil)
+             }.keyboardShortcut("q")
+         }
     }
 }
