7a4b120fb85b2fc311f0b20b79823ac4047e2120
Sheikh Bayazid <sheikhbayazid@gmail.com>
2023-08-26 02:32:10 +0200

    Initial Commit

---
+++ Derivy/DerivyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DerivyApp.swift
+//  Derivy
+//
+//  Created by Sheikh Bayazid on 26/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DerivyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
