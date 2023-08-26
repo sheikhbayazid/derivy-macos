00b4d6bda8318f97fcedf7f183b20706f8d0b608
Sheikh Bayazid <sheikhbayazid@gmail.com>
2023-08-27 00:57:47 +0200

    Add Copyright

--- Derivy/Content/ContentViewModel.swift
+++ Derivy/Content/ContentViewModel.swift
@@ -3,6 +3,7 @@
 //  Derivy
 //
 //  Created by Sheikh Bayazid on 26/8/23.
+//  Copyright © 2023 Sheikh Bayazid. All rights reserved.
 //
 
 import Combine
--- Directory/Sources/Directory/Entities/DirectoryPath.swift
+++ Directory/Sources/Directory/Entities/DirectoryPath.swift
@@ -3,6 +3,7 @@
 //  Directory
 //
 //  Created by Sheikh Bayazid on 2023-08-26.
+//  Copyright © 2023 Sheikh Bayazid. All rights reserved.
 //
 
 import Foundation
